0f6993ef11ab0f0fb0463b1e3172c155d04411cb
Utkarsh <utkarshsinghal369@gmail.com>
2023-07-25 19:53:30 +0530

    add Routes SinglePage work

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -9,6 +9,11 @@ import Mobile from "../Pages/Mobile";
 import Laptop from "../Pages/Laptop";
 import Books from "../Pages/Books";
 import Grocery from "../Pages/Grocery";
+import SingleGrocery from "../Pages/SingleGrocery";
+import SingleMobile from "../Pages/SingleMobile";
+import SingleWomenPage from "../Pages/SingleWomenPage";
+import SingleLaptop from "../Pages/SingleLaptop";
+import SingleMenPage from "../Pages/SingleMenPage";
 const AllRoutes = () => {
   return (
     <Routes>
@@ -16,11 +21,22 @@ const AllRoutes = () => {
       <Route path="/sign" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/men" element={<Men />} />
+      <Route path="/men/:id" element={<SingleMenPage />} />
+
       <Route path="/women" element={<Women />} />
+      <Route path="/women/:id" element={<SingleWomenPage />} />
+
       <Route path="/laptop" element={<Laptop />} />
+      <Route path="/laptop/:id" element={<SingleLaptop />} />
+
       <Route path="/mobile" element={<Mobile />} />
+      <Route path="/mobile/:id" element={<SingleMobile />} />
+
       <Route path="/book" element={<Books />} />
+      <Route path="/book/:id" element={<SingleBooksPage />} />
+
       <Route path="/grocery" element={<Grocery />} />
+      <Route path="/grocery/:id" element={<SingleGrocery />} />
     </Routes>
   );
 };
